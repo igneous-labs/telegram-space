8f1a86354e5e803861dc9fa0c9d13d91165364af
FP Lee <97802371+fplord@users.noreply.github.com>
2023-01-25 21:08:35 +0800

    Revert "add non working joystick"
    This reverts commit 7aa326c70c005f1215561056183e2cea202730f6.

--- client/src/scenes/character.gd
+++ client/src/scenes/character.gd
@@ -6,22 +6,22 @@ extends CharacterBody2D
 @export var character_status: Common.CharacterStatus
 
 func update_animation() -> void:
-	$AnimationPlayer.play(&"%s-%s" % [
-		Common.char_status_to_strn(self.character_status),
-		Common.dir_to_strn(self.character_direction),
-	])
+    $AnimationPlayer.play(&"%s-%s" % [
+        Common.char_status_to_strn(self.character_status),
+        Common.dir_to_strn(self.character_direction),
+    ])
 
 func update_character_state(input_vector: Vector2):
-	if input_vector.length() != 0:
-		self.character_status = Common.CharacterStatus.WALK
-		var dir = Common.vec_to_dir(input_vector)
-		if dir != Common.Direction.NONE:
-			self.character_direction = dir
-	else:
-		self.character_status = Common.CharacterStatus.IDLE
+    if input_vector.length() != 0:
+        self.character_status = Common.CharacterStatus.WALK
+        var dir = Common.vec_to_dir(input_vector)
+        if dir != Common.Direction.NONE:
+            self.character_direction = dir
+    else:
+        self.character_status = Common.CharacterStatus.IDLE
 
 func setup(initial_position: Vector2) -> void:
-	self.character_direction = Common.Direction.LEFT
-	self.character_status = Common.CharacterStatus.IDLE
-	self.position = initial_position
-	self.update_animation()
+    self.character_direction = Common.Direction.LEFT
+    self.character_status = Common.CharacterStatus.IDLE
+    self.position = initial_position
+    self.update_animation()

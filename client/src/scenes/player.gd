9151896dcfcd40f26a0020a20205822e67cd0d43
Jesse Y. Cho <jesse@socean.fi>
2023-01-25 23:16:54 +0900

    [clident] DPadController to emit Input's Action

--- client/src/scenes/player.gd
+++ client/src/scenes/player.gd
@@ -5,19 +5,19 @@ extends "res://src/scenes/character.gd"
 const SPEED: float = 200.
 
 func _physics_process(_delta: float) -> void:
-	var input_vector := Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	).normalized()
-	self.velocity = SPEED * input_vector
-	self.update_character_state(input_vector)
-	self.update_animation()
-	self.publish_player_state()
-	self.move_and_slide()
+    var input_vector := Vector2(
+        Input.get_axis("ui_left", "ui_right"),
+        Input.get_axis("ui_up", "ui_down")
+    ).normalized()
+    self.velocity = SPEED * input_vector
+    self.update_character_state(input_vector)
+    self.update_animation()
+    self.publish_player_state()
+    self.move_and_slide()
 
 func publish_player_state() -> void:
-	NetworkHandler.publish_player_state({
-		"position": self.position,
-		"direction": self.character_direction,
-		"status": self.character_status,
-	})
+    NetworkHandler.publish_player_state({
+        "position": self.position,
+        "direction": self.character_direction,
+        "status": self.character_status,
+    })

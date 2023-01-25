45ade7c1b4621a0b03e78a981520236bbfe06086
Jesse Y. Cho <jesse@socean.fi>
2023-01-25 22:47:34 +0900

    minor-edit: beta 13 -> 14

--- client/build.sh
+++ client/build.sh
@@ -3,8 +3,8 @@
 readonly BASE_DIR=$(dirname $(readlink -f "$0"))
 readonly BUILD_DIR="${BASE_DIR}/exports"
 
-# NB: change this to where godot_v4.0-beta13 binary is installed
-readonly GODOT="/path/to/godot/binary"
+# NB: change this to where godot_v4.0-beta14 binary is installed
+readonly GODOT="/opt/godot/Godot_v4.0-beta14_linux.x86_64";
 readonly TARGET="Web"
 
 mkdir -p ${BUILD_DIR}

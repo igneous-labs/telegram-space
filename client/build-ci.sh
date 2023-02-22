69a126ce3f63bf30c0769388b5752de76cb425cd
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-02-22 15:35:16 +0900

    upgrade godot 4.0 rc 2 -> rc 3

--- client/build-ci.sh
+++ client/build-ci.sh
@@ -3,7 +3,7 @@
 readonly BASE_DIR=$(dirname $(readlink -f "$0"))
 readonly BUILD_DIR="${BASE_DIR}/exports"
 
-readonly GODOT="/opt/godot/Godot_v4.0-rc2_linux.x86_64"
+readonly GODOT="/opt/godot/Godot_v4.0-rc3_linux.x86_64"
 readonly TARGET="Web"
 
 mkdir -p ${BUILD_DIR}
